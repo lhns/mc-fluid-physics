c8a609f840c479bdcc9d39c5c12af668f998dd6c
Pierre Kisters <pierrekisters@gmail.com>
2020-08-17 00:32:34 +0200

    biome dependent rain refill

--- src/main/scala/de/lolhens/minecraft/fluidphysics/config/FluidPhysicsConfig.scala
+++ src/main/scala/de/lolhens/minecraft/fluidphysics/config/FluidPhysicsConfig.scala
@@ -6,7 +6,10 @@ import io.circe.Codec
 import net.minecraft.block.Block
 import net.minecraft.fluid.{Fluid, Fluids}
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.registry.Registry
+import net.minecraft.world.World
+import net.minecraft.world.biome.Biome
 
 case class FluidPhysicsConfig(fluidWhitelist: Seq[Identifier] = Seq(Fluids.WATER, Fluids.LAVA).map(Registry.FLUID.getId),
                               findSourceMaxIterations: Int = 255,
@@ -39,10 +42,17 @@ object FluidPhysicsConfig extends Config[FluidPhysicsConfig] {
   }
 
   case class RainRefillConfig(probability: Double = 0.2,
-                              fluidWhitelist: Seq[Identifier] = Seq(Fluids.WATER).map(Registry.FLUID.getId)) {
+                              fluidWhitelist: Seq[Identifier] = Seq(Fluids.WATER).map(Registry.FLUID.getId),
+                              biomeDependent: Boolean = true) {
     lazy val getFluidWhitelist: Seq[Fluid] = fluidWhitelist.map(registryGet(Registry.FLUID, _))
 
     def canRefillFluid(fluid: Fluid): Boolean = getFluidWhitelist.exists(_.matchesType(fluid))
+
+    def canRainAt(world: World, pos: BlockPos): Boolean =
+      !biomeDependent || {
+        val biome = world.getBiome(pos)
+        biome.getPrecipitation == Biome.Precipitation.RAIN && biome.getTemperature(pos) >= 0.15F
+      }
   }
 
 }
