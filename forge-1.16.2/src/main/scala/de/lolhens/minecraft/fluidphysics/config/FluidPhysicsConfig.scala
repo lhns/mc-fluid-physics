722fafc840fd161edb48d2f6ba5791c77d0288f5
Pierre Kisters <pierrekisters@gmail.com>
2020-10-03 14:15:05 +0200

    fixed deprecations in forge ports

--- forge-1.16.2/src/main/scala/de/lolhens/minecraft/fluidphysics/config/FluidPhysicsConfig.scala
+++ forge-1.16.2/src/main/scala/de/lolhens/minecraft/fluidphysics/config/FluidPhysicsConfig.scala
@@ -7,18 +7,18 @@ import net.minecraft.block.Block
 import net.minecraft.fluid.{Fluid, Fluids}
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 import net.minecraft.world.biome.Biome
+import net.minecraftforge.registries.{ForgeRegistries, IForgeRegistry, IForgeRegistryEntry}
 
-case class FluidPhysicsConfig(fluidWhitelist: Seq[ResourceLocation] = Seq(Fluids.WATER, Fluids.LAVA).map(Registry.FLUID.getKey),
+case class FluidPhysicsConfig(fluidWhitelist: Seq[ResourceLocation] = Seq(Fluids.WATER, Fluids.LAVA).map(ForgeRegistries.FLUIDS.getKey),
                               findSourceMaxIterations: Int = 255,
                               biomeDependentFluidInfinity: Boolean = false,
                               flowOverSources: Boolean = true,
                               debugFluidState: Boolean = false,
                               spring: Option[SpringConfig] = Some(SpringConfig()),
                               rainRefill: Option[RainRefillConfig] = Some(RainRefillConfig())) {
-  lazy val getFluidWhitelist: Seq[Fluid] = fluidWhitelist.map(registryGet(Registry.FLUID, _))
+  lazy val getFluidWhitelist: Seq[Fluid] = fluidWhitelist.map(registryGet(ForgeRegistries.FLUIDS, _))
 
   def enabledFor(fluid: Fluid): Boolean = getFluidWhitelist.exists(_.isEquivalentTo(fluid))
 }
@@ -28,24 +28,24 @@ object FluidPhysicsConfig extends Config[FluidPhysicsConfig] {
 
   override protected def codec: Codec[FluidPhysicsConfig] = makeCodec
 
-  private def registryGet[A](registry: Registry[A], id: ResourceLocation): A = {
+  private def registryGet[A <: IForgeRegistryEntry[A]](registry: IForgeRegistry[A], id: ResourceLocation): A = {
     require(registry.containsKey(id), "Registry does not contain identifier: " + id)
-    registry.getOrDefault(id)
+    registry.getValue(id)
   }
 
   case class SpringConfig(block: ResourceLocation = FluidPhysicsMod.SPRING_BLOCK_ID,
                           updateBlocksInWorld: Boolean = false,
                           allowInfiniteWater: Boolean = true) {
-    lazy val getBlock: Block = registryGet(Registry.BLOCK, block)
+    lazy val getBlock: Block = registryGet(ForgeRegistries.BLOCKS, block)
 
     def shouldUpdateBlocksInWorld: Boolean =
       block != FluidPhysicsMod.SPRING_BLOCK_ID && updateBlocksInWorld
   }
 
   case class RainRefillConfig(probability: Double = 0.2,
-                              fluidWhitelist: Seq[ResourceLocation] = Seq(Fluids.WATER).map(Registry.FLUID.getKey),
+                              fluidWhitelist: Seq[ResourceLocation] = Seq(Fluids.WATER).map(ForgeRegistries.FLUIDS.getKey),
                               biomeDependent: Boolean = true) {
-    lazy val getFluidWhitelist: Seq[Fluid] = fluidWhitelist.map(registryGet(Registry.FLUID, _))
+    lazy val getFluidWhitelist: Seq[Fluid] = fluidWhitelist.map(registryGet(ForgeRegistries.FLUIDS, _))
 
     def canRefillFluid(fluid: Fluid): Boolean = getFluidWhitelist.exists(_.isEquivalentTo(fluid))
 
