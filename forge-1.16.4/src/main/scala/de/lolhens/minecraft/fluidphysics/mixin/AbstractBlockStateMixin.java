cd383b7799386d37bc9afd535c815402c98bfd5a
Pierre Kisters <pierrekisters@gmail.com>
2020-11-07 17:23:48 +0100

    fixed mixins

--- forge-1.16.4/src/main/scala/de/lolhens/minecraft/fluidphysics/mixin/AbstractBlockStateMixin.java
+++ forge-1.16.4/src/main/scala/de/lolhens/minecraft/fluidphysics/mixin/AbstractBlockStateMixin.java
@@ -2,18 +2,23 @@ package de.lolhens.minecraft.fluidphysics.mixin;
 
 import de.lolhens.minecraft.fluidphysics.FluidPhysicsMod;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.PushReaction;
 import net.minecraft.fluid.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractBlock.AbstractBlockState.class)
 public abstract class AbstractBlockStateMixin {
+    @Shadow
+    public abstract BlockState getSelf();
+
     @Inject(at = @At("HEAD"), method = "getPushReaction", cancellable = true)
     public void getPushReaction(CallbackInfoReturnable<PushReaction> info) {
-        AbstractBlock.AbstractBlockState blockState = (AbstractBlock.AbstractBlockState) (Object) this;
+        BlockState blockState = getSelf();
         FluidState fluidState = blockState.getFluidState();
         if (!fluidState.isEmpty() &&
                 FluidPhysicsMod.config().enabledFor(fluidState.getFluid()) &&
