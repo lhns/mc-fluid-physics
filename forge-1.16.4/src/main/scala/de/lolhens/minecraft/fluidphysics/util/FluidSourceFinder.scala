b95b2e5b49b12a64b7ed01e744f9d9b1e933134d
Pierre Kisters <pierrekisters@gmail.com>
2021-12-04 02:59:36 +0100

    fixes

--- forge-1.16.4/src/main/scala/de/lolhens/minecraft/fluidphysics/util/FluidSourceFinder.scala
+++ forge-1.16.4/src/main/scala/de/lolhens/minecraft/fluidphysics/util/FluidSourceFinder.scala
@@ -7,7 +7,6 @@ import net.minecraft.fluid.{FlowingFluid, Fluid, FluidState}
 import net.minecraft.util.Direction
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IWorld
-import net.minecraft.world.level.LevelAccessor
 
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
