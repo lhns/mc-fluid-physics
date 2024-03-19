799bbd9db754e4ff22bdfa80b7250cc843ffda50
Aestrak2k22 <cool.timyr2003@yandex.ru>
2024-03-19 23:48:00 +0300

    fabric 1.20.1

---
+++ fabric-1.20.1/src/main/scala/de/lolhens/minecraft/fluidphysics/mixin/AbstractBlockStateMixin.java
@@ -0,0 +1,29 @@
+package de.lolhens.minecraft.fluidphysics.mixin;
+
+import de.lolhens.minecraft.fluidphysics.FluidPhysicsMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.fluid.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public abstract class AbstractBlockStateMixin {
+    @Shadow
+    public abstract BlockState asBlockState();
+
+    @Inject(at = @At("HEAD"), method = "getPistonBehavior", cancellable = true)
+    public void getPistonBehavior(CallbackInfoReturnable<PistonBehavior> info) {
+        BlockState blockState = asBlockState();
+        FluidState fluidState = blockState.getFluidState();
+        if (!fluidState.isEmpty() &&
+                FluidPhysicsMod.config().isEnabledFor(fluidState.getFluid()) &&
+                fluidState.isStill()) {
+            info.setReturnValue(PistonBehavior.PUSH_ONLY);
+        }
+    }
+}
